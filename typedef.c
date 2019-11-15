09b90f1332bc431f2b3915cdbe35a7af963526d0
ujjal kumar maity <54595503+ujjalkumarmaity@users.noreply.github.com>
2019-11-15 16:21:19 +0530

    Add files via upload

---
+++ typedef.c
@@ -0,0 +1,12 @@
+// typedef
+/*typedef is a keyword used in C language to assign alternative names to existing datatypes.
+Its mostly used with user defined datatypes*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	typedef int ujju;
+	ujju i=1,j=2;
+	printf("sum of two number %d",i+j);
+	return 0;
+}
